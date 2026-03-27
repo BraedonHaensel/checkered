9fdb2fafe09cfefaaa9c11547f2bcd215a78772e
Avery Keuben <avery1516@gmail.com>
2026-03-27 16:19:05 -0600

    Add documentation

--- backend/utils.go
+++ backend/utils.go
@@ -4,20 +4,18 @@ import (
 	"os"
 )
 
+// Determines the proper value of a given option. Priority goes in the following order:
+// 1. `flagValue` is checked,
+// 2. `envName` is looked up as an environment variable
+// 3. `value` is used.
+// The first successful branch will be the output of the function
 func ParseStringOption(flagValue string, envName string, value string) string {
-	result := value
-	
+	if flagValue != "" { return flagValue }
+
 	if envName != "" {
-		res, found := os.LookupEnv(envName)
-
-		if found {
-			result = res
-		}
+		env, success := os.LookupEnv(envName)
+		if(success) { return env }
 	}
 
-	if flagValue != "" {
-		result = flagValue
-	}
-
-	return result
+	return value
 }
